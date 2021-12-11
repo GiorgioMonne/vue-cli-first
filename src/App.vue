9dec7d6c041b5a9aac6adcfae8c08bbc4f4a03b0
Giorgio <1999giorgio.monne@mail.com>
2021-12-10 19:37:58 -1000

    fine

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Eserc/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Eserc from './components/Eserc.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Eserc
   }
 }
 </script>
---
+++ src/components/Eserc.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="contenuto">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Eserc"
+}
+</script>
+
+<style scoped>
+    h2{
+        color: lightblue;
+    }
+</style>
